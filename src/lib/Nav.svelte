c2eacf725a5c6654f425c5fcc8e8566b04828e85
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2022-06-18 13:00:52 +0100

    init

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,4 @@
+<nav class="sticky top-0 z-40 flex flex-wrap p-3 border-slate-900/10 shadow-md rounded-b-md bg-orange-300">
+    <div class="opacity-100 z-50">   
+        <a href="/" class="p-2 mr-4 inline-flex items-center font-semibold text-xl">Some Random Portfolio</a>
+</nav>
